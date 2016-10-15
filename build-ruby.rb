d9e1f78edc75d733302b3e961ecdf88f56f63931
Koichi Sasada <ko1@atdot.net>
2016-10-15 19:37:29 +0900

    fix

--- build-ruby.rb
+++ build-ruby.rb
@@ -11,12 +11,13 @@ class BuildRuby
   def initialize repository = nil,
                  target_name = nil,
                  repository_type: nil,
-                 branch_name: nil,
+                 git_branch: nil,
+                 svn_revision: nil,
                  root_directory: "~/ruby",
                  build_opts: nil,
                  test_opts: nil,
                  steps: nil,
-                 logfile: "log.build-ruby-#{Time.now.strftime('%Y%m%d-%H%M%S')}"
+                 logfile: nil
     #
     @REPOSITORY      = repository      || 'https://svn.ruby-lang.org/repos/ruby/trunk'
     @REPOSITORY_TYPE = repository_type || find_repository_type(@REPOSITORY)
@@ -27,8 +28,10 @@ class BuildRuby
     @INSTALL_DIR = File.expand_path(File.join(root_directory, 'install'))
     @TARGET_SRC_DIR = File.join(@SRC_DIR, @TARGET_NAME)
     @TARGET_BUILD_DIR = File.join(@BUILD_DIR, @TARGET_NAME)
+    @TARGET_INSTALL_DIR = File.join(@INSTALL_DIR, @TARGET_NAME)
 
-    @branch_name = branch_name
+    @git_branch = git_branch
+    @svn_revision = svn_revision
 
     if Etc.respond_to? :nprocessors
       pn = Etc.nprocessors
@@ -53,9 +56,10 @@ class BuildRuby
       test_rubyspec
     }
 
+    logfile ||= "log.build-ruby.#{@TARGET_NAME}.#{Time.now.strftime('%Y%m%d-%H%M%S')}"
+
     STDERR.puts "Logfile: #{logfile}"
     @logfile = logfile
-    @logger = Logger.new(logfile)
   end
 
   def find_repository_type repository
@@ -70,12 +74,7 @@ class BuildRuby
   end
 
   def show_config
-    h = [:@REPOSITORY, :@REPOSITORY_TYPE, :@TARGET_NAME,
-     :@SRC_DIR, :@BUILD_DIR, :@INSTALL_DIR, :@TARGET_SRC_DIR, :@TARGET_BUILD_DIR,
-     :@build_opts, :@test_opts, :@steps, :@logfile].inject({}){|h, k|
-      h[k] = self.instance_variable_get(k); h
-    }
-    @logger.info h.inspect
+    pp self
   end
 
   def setup_dir
@@ -85,7 +84,10 @@ class BuildRuby
     FileUtils.mkdir_p(@INSTALL_DIR)        unless File.exist?(@INSTALL_DIR)
   end
 
-  def cmd *args
+  class CmdFailure < StandardError
+  end
+
+  def cmd *args, on_failure: :raise
     cmd_str = args.join(' ')
     @logger.info cmd_str
     IO.popen(cmd_str, 'r+', err: [:child, :out]){|io|
@@ -93,7 +95,20 @@ class BuildRuby
         @logger.info line.chomp
       }
     }
-    raise "command failed: #{cmd_str} (exit with #{$?.to_i})." unless $?.success?
+    @logger.info exit_str = "#{cmd_str.dump} exit with #{$?.to_i}."
+
+    if !$?.success?
+      case on_failure
+      when :raise
+        raise CmdFailure, exit_str
+      when :skip
+        @failures << exit_str
+      when :ignore
+        # ignore
+      else
+        raise
+      end
+    end
   end
 
   def checkout
@@ -103,8 +118,8 @@ class BuildRuby
       when :svn
         cmd 'svn', 'checkout', @REPOSITORY, @TARGET_NAME
       when :git
-        if @branch_name
-          cmd 'git', 'clone', '--depth', '1', '-b', @branch_name, '--single-branch', @REPOSITORY, @TARGET_NAME
+        if @git_branch
+          cmd 'git', 'clone', '--depth', '1', '-b', @git_branch, '--single-branch', @REPOSITORY, @TARGET_NAME
         else
           cmd 'git', 'clone', '--depth', '1', @REPOSITORY, @TARGET_NAME
         end
@@ -116,7 +131,9 @@ class BuildRuby
 
   def autoconf
     Dir.chdir(File.join(@SRC_DIR, @TARGET_NAME)){
-      cmd 'autoconf'
+      unless File.exist?('configure')
+        cmd 'autoconf'
+      end
     }
   end
 
@@ -136,14 +153,14 @@ class BuildRuby
   def configure
     builddir{
       unless File.exist? File.join(@TARGET_BUILD_DIR, 'Makefile')
-        cmd File.join(@TARGET_SRC_DIR, 'configure'), '--disable-install-doc', '--enable-shared', "--prefix=#{@INSTALL_DIR}"
+        cmd File.join(@TARGET_SRC_DIR, 'configure'), '--disable-install-doc', '--enable-shared', "--prefix=#{@TARGET_INSTALL_DIR}"
       end
     }
   end
 
   def build_up
     builddir{
-      cmd "make up #{@build_opts}"
+      cmd "make up #{@build_opts}", on_failure: :ignore
     }
   end
 
@@ -161,7 +178,7 @@ class BuildRuby
 
   def build_exts
     builddir{
-      cmd "make exts #{@build_opts}"
+      cmd "make exts #{@build_opts}", on_failure: :ignore
     }
   end
 
@@ -185,24 +202,28 @@ class BuildRuby
 
   def test_btest
     builddir{
-      cmd "make btest #{@test_opts}"
+      cmd "make btest #{@test_opts}", on_failure: :skip
     }
   end
 
   def test_all
     builddir{
-      cmd "make test-all #{@test_opts}"
+      cmd "make test-all #{@test_opts}", on_failure: :skip
     }
   end
 
   def test_rubyspec
     builddir{
-      cmd "make test-rubyspec #{@test_opts}"
+      cmd "make test-rubyspec #{@test_opts}", on_failure: :skip
     }
   end
 
   def run
+    @logger = Logger.new(@logfile)
+    @logger.info self.inspect
+    @failures = []
     err = nil
+
     tm = Benchmark.measure{
       Benchmark.bm(20){|x|
         @steps.each{|step|
@@ -218,18 +239,46 @@ class BuildRuby
       }
     }
     puts "total: #{'%0.2f' % tm.real} sec"
-    raise err if err
+
+    # check err
+    case err
+    when CmdFailure
+      STDERR.puts err.message
+      exit 1
+    when nil
+      # ignore
+    else
+      raise err
+    end
+
+    # check failures
+    unless @failures.empty?
+      @failures.each{|f|
+        STDERR.puts f
+      }
+      exit 1
+    end
+  end
+
+  def remove
+    FileUtils.rm_rf(p @TARGET_SRC_DIR)
+    FileUtils.rm_rf(p @TARGET_BUILD_DIR)
+    FileUtils.rm_rf(p @TARGET_INSTALL_DIR)
   end
 end
 
 opts = {}
+mode = :build
 
 opt = OptionParser.new
 opt.on('--repository_type=[TYPE]'){|type|
   opts[:repository_type] = type
 }
-opt.on('--branch_name=[BRANCH_NAME]'){|b|
-  opts[:branch_name] = b
+opt.on('-b', '--git_branch=[BRANCH_NAME]'){|b|
+  opts[:git_branch] = b
+}
+opt.on('-r', '--svn_revision=[REV]'){|r|
+  opts[:svn_revision] = r
 }
 opt.on('--build_opts=[BUILD_OPTS]'){|o|
   opts[:build_opts] = o
@@ -245,6 +294,9 @@ opt.on('--steps=["STEP1 STEP2..."]'){|steps|
 }
 opt.on('--logfile=[LOGFILE]'){|logfile|
   opts[:logfile] = logfile
+}
+opt.on('--rm'){
+  mode = :rm
 }
 opt.on('--install-only'){
   opts[:steps] = %w{
@@ -265,5 +317,11 @@ repository = ARGV.shift
 target_name = ARGV.shift
 br = BuildRuby.new(repository, target_name, **opts)
 br.show_config
-br.setup_dir
-br.run
+
+case mode
+when :build
+  br.setup_dir
+  br.run
+when :rm
+  br.remove
+end
