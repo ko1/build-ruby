c47e49feace7ff744725dd9c1cfb77d70c8f32c6
Koichi Sasada <ko1@atdot.net>
2017-03-24 14:47:46 +0900

    fix br.rb

--- br.rb
+++ br.rb
@@ -3,22 +3,66 @@ require 'optparse'
 
 cmd = ARGV.shift
 
-WORKING_DIR = "~/ruby"
+WORKING_DIR = File.expand_path(ENV['BUILD_RUBY_WORKING_DIR'] || "~/ruby")
 BUILD_RUBY_SCRIPT = File.join(File.dirname(__FILE__), 'build-ruby.rb')
-case cmd
-when 'list'
+
+def build target
+  target_file = File.expand_path(File.join(WORKING_DIR, "#{target}.br"))
+
+  # opts by default
+  opts << "--target_name=#{target}"
+  logfile = File.join(WORKING_DIR, 'logs', "brlog.#{target}.#{Time.now.strftime('%Y%m%d-%H%M%S')}")
+  opts << "--logfile=#{logfile}"
+
+  # opts from config file
+  opts << open(target_file){|f|
+    f.readlines.map{|line| line.chomp}.join(' ')
+  }
+
+  # opts from command line
+  opts << ARGV.join(' ')
+
+  system("ruby #{BUILD_RUBY_SCRIPT} #{opts}")
+  [$?, logfile]
+end
+
+def target_configs
   Dir.glob(File.join(WORKING_DIR, '*.br')){|file|
-    puts File.basename(file, '.br')
+    yield file
+  }
+end
+
+def run target
+  raise "run is unsupported yet"
+end
+
+case cmd
+when nil, '-h', '--help'
+  puts <<EOS
+br.rb: supported commands
+  * list
+  * build [target]
+  * build_all
+  * run [target]
+  * run_all
+EOS
+
+when 'list'
+  target_configs{|target_config|
+    puts File.basename(target_config, '.br')
   }
 when 'build'
-  target = ARGV.shift || raise('build target is not provided')
-  target_file = File.expand_path(File.join(WORKING_DIR, "#{target}.br"))
-  opts = open(target_file){|f|
-    f.readlines.map{|line| line.chomp}.join(' ')
-  } + ARGV.join(' ')
-  p "ruby #{BUILD_RUBY_SCRIPT} #{opts}"
-when nil
-  raise "command is not provided"
+  build ARGV.shift || raise('build target is not provided')
+when 'build_all'
+  target_configs{|target_config|
+    build File.basename(target_config, '.br')
+  }
+when 'run'
+  run ARGV.shift
+when 'run_all'
+  target_configs{|target_config|
+    run File.basename(target_config, '.br')
+  }
 else
   raise "#{cmd} is not supported"
 end
