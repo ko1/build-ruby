3bf4db433dd9579b4dca9db6b4f9c7aff93e85eb
silicon <ko1@silicon.com>
2020-06-09 14:55:12 +0900

    add Ubuntu Focal

--- docker/run.rb
+++ docker/run.rb
@@ -1,12 +1,10 @@
 
-IMAGE_NAME='rubydev:bionic'
-
-def run config_name, noop = false
+def run image, config_name, noop = false
   cmd = "docker run --init -d --rm -v ~/ruby:/home/ko1/ruby " + 
         "--name=#{config_name} --hostname=#{`hostname`.strip}-docker " +
         "--cap-add=SYS_PTRACE --tmpfs /tmp:exec " +
         "-e BUILD_RUBY_WORKING_DIR=/tmp/ruby " +
-        "#{IMAGE_NAME} " +
+        "#{image} " +
         "ruby #{__dir__}/boot.rb #{config_name} --incremental --process-num=6"
   puts "kick: #{cmd}"
   system cmd unless noop
@@ -25,7 +23,9 @@ end
 if ARGV.empty?
 
            # trunk-asserts-nopara 
-tests = %w(trunk_gcc4 trunk_gcc5 trunk_gcc6 trunk_gcc7 trunk_gcc8 trunk_gcc9
+tests = {
+  'rubydev:bionic' =>
+        %w(trunk_gcc4 trunk_gcc5 trunk_gcc6 trunk_gcc7 trunk_gcc8
            trunk_clang_39 trunk_clang_40 trunk_clang_50 trunk_clang_60
            trunk_clang_7 trunk_clang_8
            trunk-nopara
@@ -41,34 +41,47 @@ tests = %w(trunk_gcc4 trunk_gcc5 trunk_gcc6 trunk_gcc7 trunk_gcc8 trunk_gcc9
            trunk-gc_compact
            trunk-O0
            trunk-cross-mingw64
-        )
+        ),
+  'rubydev:focal' =>
+        %w(trunk_gcc9 trunk_gcc10
+	   trunk_clang_9 trunk_clang_10
+        ),
+}
+
 
 # clean
 if ENV['CLEAN_ALL'] == 'true'
-  tests.each{|test|
-    system("docker kill #{test}")
-    system("ruby ~/ruby/build-ruby/br.rb build #{test} --rm=all")
+  tests.each{|image, tests|
+    tests.each{|test|
+      system("docker kill #{test}")
+      system("ruby ~/ruby/build-ruby/br.rb build #{test} --rm=all")
+    }
   }
   exit
 end
 
 if ENV['NOOP'] == 'true'
-  tests.each{|test|
-    run test, true
+  tests.each{|image, tests|
+    tests.each{|test|
+      run image, test, true
+    }
   }
   exit
 end
 
 ps = `docker ps --no-trunc`
-tests.each{|t|
-  if /^(.+#{t}.+)$/ =~ ps
-    STDERR.puts "#{t} is already launched: #{$1}"
-  else
-    run t
-    st = rand(30)
-    puts "sleep #{st}"
-    sleep rand(st)
-  end
+
+tests.each{|image, tests|
+  tests.each{|test|
+    if /^(.+#{test}.+)$/ =~ ps
+      STDERR.puts "#{test} is already launched: #{$1}"
+    else
+      run image, test
+      st = rand(30)
+      puts "sleep #{st}"
+      sleep rand(st)
+    end
+  }
 }
 
 else
