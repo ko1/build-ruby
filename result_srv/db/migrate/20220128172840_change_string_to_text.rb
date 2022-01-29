328e44bbf5c76066f0ef0a8b2ebc1987b09ec495
Koichi Sasada <ko1@atdot.net>
2022-01-30 03:30:41 +0900

    cancel to convert types
    It takes too long time...

--- result_srv/db/migrate/20220128172840_change_string_to_text.rb
+++
@@ -1,7 +0,0 @@
-class ChangeStringToText < ActiveRecord::Migration[7.0]
-  def change
-    change_column :results, :memo, :text
-    change_column :results, :desc, :text
-    change_column :results, :desc_json, :text
-  end
-end
