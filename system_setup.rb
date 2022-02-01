b55e837784fd381763ec0dc39ef2cff1b09500b9
Koichi Sasada <ko1@atdot.net>
2022-02-01 17:05:57 +0900

    add system_setup.rb

---
+++ system_setup.rb
@@ -0,0 +1,17 @@
+core_pattern = '/tmp/cores/core.%u.%p.%e'
+
+open("/etc/sysctl.d/99-ci_rvm_setup.conf", 'w') do |f|
+  f.puts <<~EOC
+  #
+  # Setup for ci.rvm.jp
+  #
+  
+  kernel.yama.ptrace_scope = 1
+  kernel.core_pattern = #{core_pattern}
+  EOC
+end
+
+system("echo #{core_pattern} > /proc/sys/kernel/core_pattern")
+
+Dir.mkdir('/tmp/cores')
+File.chmod(077, '/tmp/cores')
