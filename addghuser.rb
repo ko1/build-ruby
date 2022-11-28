1ab6c3aa72ecdc1e3b59991a26f465f76b7553e1
Koichi Sasada <ko1@atdot.net>
2022-11-29 05:48:40 +0900

    add adduser util

---
+++ addghuser.rb
@@ -0,0 +1,19 @@
+
+require 'fileutils'
+require 'open-uri'
+
+ARGV.each{|user|
+  p password = rand.to_s
+  p keys = URI.open("https://github.com/#{user}.keys").read
+
+  system("adduser #{user} --disabled-password --gecos \"\"") or raise
+  system("echo #{user}:'#{password}' | /usr/sbin/chpasswd") or raise
+
+  homedir = "/home/#{user}"
+
+  File.open(homedir + "/password", 'w', 0400){|f| f.puts password}
+  Dir.mkdir(sshdir = homedir + "/.ssh", 0700)
+  File.open(sshdir + "/authorized_keys", 'w', 0400){|f| f.puts keys}
+
+  FileUtils.chown_R(user, nil, homedir)
+}
