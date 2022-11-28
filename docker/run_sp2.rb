5beac28e33db6cf49b51ef1277b55afc03b638c2
Koichi Sasada <ko1@atdot.net>
2022-11-29 03:39:59 +0900

    docker config for ruby-sp2

---
+++ docker/run_sp2.rb
@@ -0,0 +1,98 @@
+
+def kick image, cmd, config_name, run_opt, noop
+  cmd = "docker run #{run_opt} --memory=4g --init --rm " +
+        "-v #{__dir__}/ruby:/home/ko1/ruby " + 
+        "-v #{__dir__}/../:/home/ko1/build-ruby " + 
+        "--name=#{config_name} --hostname=#{`hostname`.strip}-docker " +
+        "--cap-add=SYS_PTRACE --tmpfs /tmp:exec " +
+        "-e BUILD_RUBY_WORKING_DIR=/tmp/ruby " +
+        "#{image} #{cmd}"
+  puts "kick: #{cmd}"
+  system cmd unless noop
+end
+
+def run image, config_name, noop = false
+  kick image, "ruby /home/ko1/ruby/boot.rb #{config_name} --incremental --process-num=4", config_name, '-d', noop
+end
+
+def run_interractive arg, noop = false
+  image = 'rubydev:jammy'
+  kick image, arg, "run_interractive.#{$$}", '-it', noop
+end
+
+if ARGV.empty?
+
+tests = {
+  'rubydev:jammy' => %w{
+    trunk-random0
+    trunk-random1
+    trunk-random2
+    trunk-random3
+    trunk-repeat20
+    trunk-repeat20-asserts
+    trunk-repeat50
+    trunk-random-repeat
+
+    trunk-gc-asserts
+    trunk-asserts
+    trunk-iseq_binary
+    trunk-no-mjit
+    trunk-O0
+
+    trunk_gcc9
+    trunk_gcc10
+    trunk_gcc11
+    trunk_gcc12
+    trunk_clang_11
+    trunk_clang_12
+    trunk_clang_13
+    trunk_clang_14
+
+    trunk-jemalloc
+
+    trunk-yjit
+    trunk-yjit-asserts
+  },
+}
+
+# clean
+if ENV['CLEAN_ALL'] == 'true'
+  tests.each{|image, tests|
+    tests.each{|test|
+      system("docker kill #{test}")
+      system("ruby ~/ruby/build-ruby/br.rb build #{test} --rm=all")
+    }
+  }
+  exit
+end
+
+if ENV['NOOP'] == 'true'
+  tests.each{|image, tests|
+    tests.each{|test|
+      run image, test, true
+    }
+  }
+  exit
+end
+
+ps = `docker ps --no-trunc`
+
+tests.each{|image, tests|
+  tests.each{|test|
+    if /^(.+#{test}.+)$/ =~ ps
+      STDERR.puts "#{test} is already launched: #{$1}"
+    else
+      run image, test
+      st = rand(30)
+      puts "sleep #{st}"
+      sleep rand(st)
+    end
+  }
+}
+
+else
+  run_interractive ARGV.join(' ')
+end
+
+
+
